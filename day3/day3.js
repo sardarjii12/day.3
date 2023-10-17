ed50a17fa685b73550b0b209940589211a63bf83
sardarjii12 <148244550+sardarjii12@users.noreply.github.com>
2023-10-17 18:42:38 +0000

    today

---
+++ day3/day3.js
@@ -0,0 +1,32 @@
+console.log ("happy")
+// console.log (2+2)
+// console.log (2/2)
+// console.log (2-2)
+// console.log (2*2)
+// console.log (2%2)
+
+
+
+// console.log (2%1)
+// console.log (2%1)
+// console.log (2%1)
+// console.log (2%1)
+// console.log (2%1)
+
+console.log("2"+3+1)
+console.log(2+3+"1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
