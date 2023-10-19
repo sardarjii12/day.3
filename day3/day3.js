f356bc8aa3aba8cbe99d6efc6fde6dd00674e046
sardarjii12 <148244550+sardarjii12@users.noreply.github.com>
2023-10-19 05:49:33 +0000

    mm

--- day3/day3.js
+++ day3/day3.js
@@ -1,24 +1,20 @@
-console.log ("happy")
-// console.log (2+2)
-// console.log (2/2)
-// console.log (2-2)
-// console.log (2*2)
-// console.log (2%2)
+// console.log ("happy")
+// // console.log (2+2)
+// // console.log (2/2)
+// // console.log (2-2)
+// // console.log (2*2)
+// // console.log (2%2)
 
 
 
-// console.log (2%1)
-// console.log (2%1)
-// console.log (2%1)
-// console.log (2%1)
-// console.log (2%1)
+// // console.log (2%1)
+// // console.log (2%1)
+// // console.log (2%1)
+// // console.log (2%1)
+// // console.log (2%1)
 
-console.log("2"+3+1)
-console.log(2+3+"1")
-
-
-
-
+// console.log("2"+3+1)
+// console.log(2+3+"1")
 
 
 
@@ -27,6 +23,36 @@ console.log(2+3+"1")
 
 
 
+// let names =["sandeep,happy,gagan"]
+// console.log(names);
+// let detal={
+//     namess:"gagan",
+//     age:25,
+//     class:"wd",
+// }
+// console.log(detal.namess);
+
+// const temp = null
+// console.log(temp);
+// let bignumber = 2252222555544445223566566
 
 
+// console.log(typeof bignumber);
+// console.log(typeof temp);
 
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let myname = "charnjeet"
+let othername =myname
+othername = "gagan"
+console.log(othername);
+console.log(myname)
+
+
+let detal={
+    namess:"gagan",
+        age:25,
+     class:"wd",
+    }
+    let detal1{}
