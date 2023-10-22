c1e422a3e1dc71a5085023f9cf132ceee39c721a
sardarjii12 <148244550+sardarjii12@users.noreply.github.com>
2023-10-22 19:07:57 +0000

    big

--- day3/day3.js
+++ day3/day3.js
@@ -43,16 +43,58 @@
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-let myname = "charnjeet"
-let othername =myname
-othername = "gagan"
-console.log(othername);
-console.log(myname)
+// let myname = "charnjeet"
+// let othername =myname
+// othername = "gagan"
+// console.log(othername);
+// console.log(myname)
 
 
-let detal={
-    namess:"gagan",
-        age:25,
-     class:"wd",
-    }
-    let detal1{}
+// let detal={
+//     namess:"gagan",
+//         age:25,
+//      class:"wd",
+//     }
+// console.log("detal");
+
+// const name 
+  
+
+
+
+// // string 
+
+
+
+// const name = "charnjeet singh"
+// const age = "25"
+
+
+// console.log("name+age");
+// console.log( `my name is ${name}, my age is ${age}` )
+
+
+// const gameName = new String("charn")
+// console.log(gameName);
+// console.log(gameName.length);
+// console.log(gameName.valueOf);
+// let accountid = 4584
+// console.log(accountid);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt('3'))
+// console.log(gameName.charAt('4'))
+// console.log(gameName.charAt('2'))
+// console.log(gameName.indexOf('n'))
+// console.log(gameName.indexOf('r'))
+// console.log(gameName.substring(0, 3))
+// console.log(gameName.slice(-5, 2))
+
+
+const newfile = "    gagandeep    "
+console.log(newfile.trim());
+
+let url = "http://charnjeet.on.in/verr%20c jpdhtiii"
+
+console.log(url.replace('%20', '-'));
+console.log(url.replace(' ', '/'));
